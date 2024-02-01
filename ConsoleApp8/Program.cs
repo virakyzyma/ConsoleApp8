1fb1ae416d32acc3b283e76d7f18f6de3f0d37d3
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-02-01 16:51:38 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = "x w x y x w";
+            int x = 0, w = 0, y = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'x')
+                {
+                    x++;
+                }
+                else if (str[i] == 'w')
+                {
+                    w++;
+                }
+                else if (str[i] == 'y')
+                {
+                    y++;
+                }
+            }
+            Console.WriteLine($"X-> {x}\tY-> {y}\tW-> {w}");
+        }
+    }
+}
